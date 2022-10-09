a6a87667214d4916b9eedc441b8672a728a4ece5
VaniSan <valonsos1700@alumno.ipn>
2022-10-09 15:18:25 -0500

    Guardando 'funciones02'

--- funciones02.py
+++ funciones02.py
@@ -1,27 +1,28 @@
 def promParcial():
     califPar = []
+    promParMat = 0
+
     for i in range(0,3):
         califPar.append(float(input("Ingresa tu calificacion del parcial "+str(i+1)+"\n")))
 
-    promParMat = 0
     for j in califPar:
         promParMat += j        
     return (promParMat/3)
 
 def promGeneral(nmat):
     califMat = []
+    sumM = 0
+
     for i in range(0,nmat):
         print("Calificaciones de la materia "+str(i+1))
         califMat.append(promParcial())
 
-    sumM = 0
     for i in califMat:
         sumM += i
     return(sumM/nmat)
     
 
-def main(): 
-    
+def menu(): 
     acc = True
     while(acc):
         nmat =int(input("Bienvenido, cuantas materias tienes?:\n"))
@@ -29,5 +30,8 @@ def main():
         print("Tu Promedio escolar fue: "+str(resultProm))
         if(input("Le gustaria continuar? \n1) Si\n2)No\n") == '2'):
             acc = False
+
+def main():
+    menu()
 
 main()
